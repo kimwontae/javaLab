9c29e940dfc6692ed42b6570f720c16f9e2d1e40
uuontae <uuontae@gmail.com>
2021-09-16 18:15:04 +0900

    .

--- Example/src/BoardExample/BoardMain.java
+++ Example/src/BoardExample/BoardMain.java
@@ -2,7 +2,7 @@ package BoardExample;
 
 import java.util.Scanner;
 
-public class BoardMain {
+public class BoardMain {//
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
